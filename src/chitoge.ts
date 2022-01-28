64189cdeb87b5b1a16061fab0aa9458130080df9
Shinei Ichijo <85594230+ShineiIchijo@users.noreply.github.com>
2022-01-28 10:16:54 +0530

    Update chitoge.ts

--- src/chitoge.ts
+++ src/chitoge.ts
@@ -112,7 +112,7 @@ db.once("open", () => {
     messageHandler.spawnCards();
     newsHandler.broadcastNews();
   });
-  client.once("connecting", () => {
+  client.once("connection-phone-change", ({}) => {
     messageHandler.sendReconnectMessage()
   })
 });
